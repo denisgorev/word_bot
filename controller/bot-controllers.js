615c8fbe9acfe157b1a4364546e914518ea1df4d
Денис Горев <denisgorev1992@gmail.com>
2023-07-08 01:49:35 +0200

    fixed some bugs

--- controller/bot-controllers.js
+++ controller/bot-controllers.js
@@ -4,34 +4,31 @@ const { call } = require("../google-engine/google-api");
 const bot = new Telegraf(process.env.TOKEN);
 
 const wordBot = () => {
-  bot.start( (ctx) => {
-    ctx.replyWithHTML(
-      `Привет, ${ctx.from.first_name}! Давай изучать слова!`
-    );
+  bot.start((ctx) => {
+    ctx.replyWithHTML(`Привет, ${ctx.from.first_name}! Давай изучать слова!`);
   });
 
-bot.command("allwords", async (ctx) => {
-    const text = []
+  bot.command("allwords", async (ctx) => {
+    const text = [];
     try {
-    const response = await call()
-    } catch (err) {
-        console.log(err);
-        process.exit(1)
-    }
-    const wordArray = response.values
-    for (i in wordArray) {
-        if (i != 0){
-            let value = ''
-            value = wordArray[i][0] + ': ' + wordArray[i][1] + ' \n'
-            // ctx.replyWithHTML(value)
-            text.push(value)
-            
-        }
-        
-    }
+      const response = await call();
 
-    ctx.replyWithHTML(text.join("").toString())
-})
+      const wordArray = response.values;
+      for (i in wordArray) {
+        if (i != 0) {
+          let value = "";
+          value = wordArray[i][0] + ": " + wordArray[i][1] + " \n";
+          // ctx.replyWithHTML(value)
+          text.push(value);
+        }
+      }
+
+      ctx.replyWithHTML(text.join("").toString());
+    } catch (err) {
+      console.log(err);
+      process.exit(1);
+    }
+  });
 
   bot.launch();
 };
--- google-engine/google-api.js
+++ google-engine/google-api.js
@@ -1,6 +1,5 @@
 const call = async () => {
   const credentials = JSON.parse(process.env.keys);
-  console.log(credentials);
   const { google } = require("googleapis");
   const SCOPES = ["https://www.googleapis.com/auth/spreadsheets"];
   const auth = new google.auth.GoogleAuth({
@@ -13,22 +12,20 @@ const call = async () => {
       version: "v4",
       auth: authClientObject,
     });
+    
     const spreadsheetId = "1I275HAkbd8W9bPwwl9kV0pnscdQ_0b38aMoncSjEYuY";
-  } catch (err) {
-    console.log(err);
-  }
-  try {
     const readData = await googleSheetsInstance.spreadsheets.values.get({
       auth, //auth object
       spreadsheetId, // spreadsheet id
       range: "Words!A:B", //range of cells to read from.
     });
-  } catch (err) {
-    console.log(err);
-  }
+
 
   // console.log(readData.data);
   return readData.data;
+} catch (err) {
+    console.log(err);
+  }
 };
 
 module.exports = {
